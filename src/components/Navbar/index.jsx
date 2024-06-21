76037ccfbcc28f77cbc7a69a4827cbeaae12faf1
Eli-Jiron <elianguevara28@gmail.com>
2024-06-21 13:30:31 -0600

    Funcion: registro y validaciones

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <ul>
+            <li>
+              <Link to={"/home"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+            <li>
+              <Link to={"/register"}>Register</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,56 @@
-const Register = () => {
-  return (
-    <h1>Register</h1>
-  )
-}
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { validar } from "../js/validaciones.js";
+import { postData } from "../js/fetch.js";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
-export default Register
+const Register = () => {
+  const [mensaje, setMensaje] = useState();
+  const navigate = useNavigate();
+  const inputUser = useRef();
+  const inputPassword = useRef();
+
+  const registro = async (user, password) => {
+    if (validar.vacio(user, password)) {
+      setMensaje("Debe llenar todos los campos");
+    } else if (validar.espacios(user, password)) {
+      setMensaje("No ingrese espacios");
+    } else if (validar.username(user)) {
+      setMensaje("Nombre de usuario no valido ( A-z, números, '.' o '_' )");
+    } else if (await validar.usuario(user)) {
+      setMensaje("El nombre de usuario no está disponible");
+    } else {
+      let dataReg = {
+        user: user,
+        password: password,
+        tasks: [],
+      };
+      await postData(dataReg);
+      setMensaje("Se ha registrado");
+      setTimeout(() => {
+        navigate("/login");
+      }, 1000);
+    }
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <div>
+        <h2>Registrese</h2>
+        <Input inputRef={inputUser} txt="Usuario" />
+        <Input inputRef={inputPassword} txt="Contraseña" />
+        <div>{mensaje}</div>
+        <Button
+          foo={() =>
+            registro(inputUser.current.value, inputPassword.current.value)
+          }
+          txt="Registrarse"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Register;
